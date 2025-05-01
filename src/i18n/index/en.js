3551c7b6fa8b6fa4132222d3fb67283a178cc254
JonatanFD <u20211b995@upc.edu.pe>
2025-05-01 13:28:08 -0500

    feat: add i18n

---
+++ src/i18n/index/en.js
@@ -0,0 +1,19 @@
+
+const en = {
+    "header": {
+        "nav" : {
+            "about": "About",
+            "faq": "FAQ",
+            "team": "Team"
+        },
+        "userButtons": {
+            "signUp": "Sign Up",
+            "signIn": "Sign In"
+        }
+    },
+    "home": {
+        "title": "Design Your Custom T-Shirt in Minutes",
+        "description": "No design skills needed — just choose, customize, and create your own t-shirt with our intuitive editor.",
+        "callToAction": "Start Designing Now",
+    }
+}
---
+++ src/i18n/index/es.js
@@ -0,0 +1,19 @@
+
+const es = {
+    "header": {
+        "nav" : {
+            "about": "Acerca de",
+            "faq": "FAQ",
+            "team": "Equipo"
+        },
+        "userButtons": {
+            "signUp": "Registrarse",
+            "signIn": "Iniciar sesión"
+        }
+    },
+    "home": {
+        "title": "Diseña tu camiseta personalizada en minutos",
+        "description": "No se necesitan habilidades de diseño: solo elige, personaliza y crea tu propia camiseta con nuestro editor intuitivo.",
+        "callToAction": "Comienza a diseñar ahora",
+    }
+}
