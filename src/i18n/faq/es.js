dd3c0cc270d865c727703426f8e43a6ee95c4415
JonatanFD <u20211b995@upc.edu.pe>
2025-05-04 17:29:12 -0500

    finished

---
+++ src/i18n/faq/es.js
@@ -0,0 +1,42 @@
+
+const es = {
+    "header": {
+        "nav" : {
+            "about": "Acerca de",
+            "faq": "FAQ",
+            "team": "Equipo"
+        },
+        "userButtons": {
+            "signUp": "Registrarse",
+            "signIn": "Iniciar sesión"
+        }
+    },
+    "footer": {
+        "homePage": {
+            "title": "Página de inicio",
+            "community": "Comunidad",
+            "company": "Compañía"
+        },
+        "company": {
+            "title": "Compañía",
+            "about": "Acerca de",
+            "team": "Equipo",
+            "contactUs": "Contáctanos"
+        },
+        "community": {
+            "title": "Comunidad",
+            "faq": "FAQ",
+            "blog": "Blog",
+            "support": "Soporte"
+        },
+        "socialMedia": {
+            "title": "Redes sociales"
+        },
+        "bottom": {
+            "title": "Quri",
+            "description": "Tu espacio creativo te está esperando. <a href=\"#\">Inicia sesión </a>para comenzar",
+            "privacyPolicy": "Política de privacidad",
+            "termsOfService": "Términos de servicio"
+        }
+    }   
+}
---
+++ src/i18n/team/en.js
@@ -0,0 +1,36 @@
+
+const en = {
+    "header": {
+        "nav" : {
+            "about": "About",
+            "faq": "FAQ",
+            "team": "Team"
+        },
+        "userButtons": {
+            "signUp": "Sign Up",
+            "signIn": "Sign In"
+        }
+    },
+    "footer": {
+        "homePage": {
+            "title": "Home Page",
+            "community": "Community",
+            "company": "Company"
+        },
+        "company": {
+            "title": "Company",
+            "about": "About",
+            "team": "Team",
+            "contactUs": "Contact Us"
+        },
+        "socialMedia": {
+            "title": "Social Media"
+        },
+        "bottom": {
+            "title": "Quri",
+            "description": "Your creative space is waiting. <a href=\"#\">Sign in </a>to get started",
+            "privacyPolicy": "Privacy Policy",
+            "termsOfService": "Terms of Service"
+        }
+    }
+}
